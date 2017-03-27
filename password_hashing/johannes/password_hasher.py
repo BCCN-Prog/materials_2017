59e7a0969d997f2dc6b7f75c8b681a894da07d39
Johannes Rieke <johannes.rieke@gmail.com>
2017-03-27 16:48:33 +0200

    Store passwords in json file

--- password_hashing/johannes/password_hasher.py
+++ password_hashing/johannes/password_hasher.py
@@ -1,2 +1,21 @@
+from getpass import getpass
+import json
+
+try:
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+except EnvironmentError:
+    users = {}
+
 username = input('Enter your username: ')
-password = input('Enter your password: ')
+password = getpass('Enter your password: ')
+
+if username in users:
+    print("Modified password for exisiting user:", username)
+else:
+    print("Created new user:", username)
+
+users[username] = password
+
+with open('users.json', 'w') as f:
+    json.dump(users, f, sort_keys=True, indent=4)
