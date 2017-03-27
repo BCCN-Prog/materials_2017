29f27dd1370bbccdef09a04b8ef266a40b6da02d
Claudia Winklmayr <claudiaw@bccn-berlin.de>
2017-03-27 16:33:56 +0200

    save username and passwords to dictionary.

--- password_hashing/claudia_eric/password_hasher.py
+++ password_hashing/claudia_eric/password_hasher.py
@@ -1,5 +1,22 @@
 from getpass import getpass
+import pickle
+import os.path
+from pathlib import Path
+
+
+UserData = Path('/home/newuser/Dokumente/programming_project/materials_2017/password_hashing/claudia_eric/UsersPasswords.p')
+
+if UserData.is_file():
+    users_and_passwords = pickle.load( open( "UsersPasswords.p", "rb" ) )
+    print(users_and_passwords)
+else:
+    users_and_passwords = {}
 
 username = input('Enter your username: ')
 password = getpass('Enter your password: ')
+
+users_and_passwords[username] = password
+print(users_and_passwords)
+
+pickle.dump(users_and_passwords, open( "UsersPasswords.p", "wb" ))
 
