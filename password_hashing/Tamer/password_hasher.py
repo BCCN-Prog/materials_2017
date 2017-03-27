69db1b21c0cb328d1e3d127c7818e796b3cb7803
t-ajaj <tamer.ajaj@gmail.com>
2017-03-27 10:29:42 -0400

    added dictionary for passwords

--- password_hashing/Tamer/password_hasher.py
+++ password_hashing/Tamer/password_hasher.py
@@ -1,4 +1,21 @@
 from getpass import getpass
+import pickle
+quit = True
+while quit==True:
+	username = raw_input("Enter your username: ")
+	password = getpass("Enter your password: ")
 
-username = raw_input("Enter your username: ")
-password = getpass("Enter your password: ")
+	print (username)
+	print (password)
+
+
+
+
+	pass_dictionary = {}
+	pass_dictionary[username] = password
+	test=raw_input("Enter Q to quit: ")
+	if test=="q": quit =False
+output = open('myfile.pkl', 'wb')
+pickle.dump(pass_dictionary, output)
+output.close()
+
