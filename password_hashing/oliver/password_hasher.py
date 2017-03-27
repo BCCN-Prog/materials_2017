22f7d230fa8e11df00c1d9b21dcf8eed53b36f13
OliverAtanaszov <oliver.atanaszov@gmail.com>
2017-03-27 16:33:00 +0200

    noidea

--- password_hashing/oliver/password_hasher.py
+++ password_hashing/oliver/password_hasher.py
@@ -1,16 +1,21 @@
 from getpass import getpass
 from pprint import pprint
-users = {}
+import sys
 
+fname = 'usersdata.txt'
+users = {}
 counter = 0
 while (counter < 2):
-
     username = input('Enter your username: ')
     password = getpass('Enter your password: ')
     users[username]=password
     counter += 1
-with open('usersdata.txt', 'w') as f:
+
+try:
+    f = open(fname, 'w')
+except IOError:
+    print("Could not write to file: {}".format(fname))
+    raise
+with f:
     for user, pw in users.items():
         f.write("{} {}\n".format(user,pw))
-
-pprint(users)
