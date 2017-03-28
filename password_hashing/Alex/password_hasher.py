d53f3762583a94db5d02c028b3666b166daba363
Alex Moore <zandermoore1994@outlook.com>
2017-03-28 10:54:49 +0200

    Changes to password function

--- password_hashing/Alex/password_hasher.py
+++ password_hashing/Alex/password_hasher.py
@@ -1,4 +1,10 @@
 from getpass import getpass
 
 username = input('Enter your username: ')
-password = input('Enter your password: ')
+password = getpass('Enter your password: ')
+
+
+dictionary = {username:password}
+d=open("password_dictionary","a+")
+for i in dictionary:
+    d.write("{0:s},{1:s}\n".format(i,dictionary[i]))
