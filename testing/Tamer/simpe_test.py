2ad85a8f5a76ca96516da3972de8370143338fb7
t-ajaj <tamer.ajaj@gmail.com>
2017-03-28 07:56:46 -0400

    end of

--- testing/Tamer/simpe_test.py
+++ testing/Tamer/simpe_test.py
@@ -11,9 +11,10 @@ def test_exercise_example_1():
 
 def test_exercise_example_2():
 
-    x = [-i**2 for i in range(-3, 4)]
+    x = [4, 2, 1, 3, 1, 2]
+
     result = find_maxima(x)
-    correct_results = [3]
+    correct_results = [0,3,5]
     assert result == correct_results
 
      
@@ -21,18 +22,68 @@ def test_exercise_example_2():
 
 def test_exercise_example_3():
 
-    x = [np.sin(2*alpha) for alpha in np.linspace(0.0, 5.0, 100)]
+    x = [4, 2, 1, 3, 1, 5]
+
     result = find_maxima(x)
-    correct_results = [3]
+    correct_results = [0,3,5]
     assert result == correct_results
+
 
 
 def test_exercise_example_4():
 
-    x = [4, 2, 1, 3, 1, 2]
+    x = [4, 2, 1, 3, 1]
     result = find_maxima(x)
-    correct_results = [3]
+    correct_results = [0,3]
+    print (result)
     assert result == correct_results
 
 
 
+def test_exercise_example_5():
+
+    x = [1, 2, 2, 1]
+
+    result = find_maxima(x)
+    correct_results = [1,2]
+    print (result)
+    assert result == correct_results
+
+def test_exercise_example_6():
+
+
+    x = [1, 2, 2, 3, 1]
+    result = find_maxima(x)
+    correct_results = [1,3]
+    print (result)
+    assert result == correct_results
+
+def test_exercise_example_7():
+
+
+    x = [1, 3, 2, 2, 1]
+    result = find_maxima(x)
+    correct_results = [1,3]
+    print (result)
+    assert result == correct_results
+
+def test_exercise_example_8():
+
+
+    x = [3, 2, 2, 3]
+    result = find_maxima(x)
+    correct_results = [0,3]
+    print (result)
+    assert result == correct_results
+
+
+    
+
+test_exercise_example_1()
+test_exercise_example_2()
+test_exercise_example_3()
+test_exercise_example_4()
+test_exercise_example_5()
+test_exercise_example_6()
+test_exercise_example_7()
+test_exercise_example_8()
