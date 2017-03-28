aabbfb8712d8082eb92fd3da90a8442b6aab764f
NeuroDebian User <brain@neurodebian>
2017-03-28 07:55:08 -0400

    functions

--- testing/simpe_test.py
+++ testing/simpe_test.py
@@ -1,7 +1,7 @@
 from maxima import find_maxima
 
 def test_exercise_example_1():
-    x = [0, 1, 2, 1, 2, 1, 0]
+    x = [4,2,1,3,1,2]
     result = find_maxima(x)
     correct_results = [2, 4]
     assert result == correct_results
