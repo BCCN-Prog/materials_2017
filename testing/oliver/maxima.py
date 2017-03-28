c5fcd122975b8f479600570a31ea12a5ffd7934f
OliverAtanaszov <oliver.atanaszov@gmail.com>
2017-03-28 10:15:32 +0200

    Fixed find_max

--- testing/oliver/maxima.py
+++ testing/oliver/maxima.py
@@ -19,8 +19,13 @@ def find_maxima(x):
 
     idx = []
     for i in range(len(x)):
-        # `i` is a local maximum if the signal decreases before and after it
-        if x[i-1] < x[i] and x[i+1] < x[i]:
+        if i==0 and x[i] > x[i+1]:
+            idx.append(i)
+
+        elif i==len(x)-1 and x[i] > x[i-1]:
+            idx.append(i)
+
+        elif ((1 < i < len(x)-1) and (x[i-1] < x[i]) and (x[i+1] < x[i])):
             idx.append(i)
     return idx
 
