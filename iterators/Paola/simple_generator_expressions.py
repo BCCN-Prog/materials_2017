06b96c87fe3d932a986df80ab8112b91c8055552
Paola Suárez <pao.2702@gmail.com>
2017-03-29 10:35:49 +0200

    Notes on iterators and generators

--- iterators/Paola/simple_generator_expressions.py
+++ iterators/Paola/simple_generator_expressions.py
@@ -5,6 +5,9 @@ S = [x**2 for x in range(10)]
 print(S)
 print(sum(S))
 
+# The only difference is the round brackets.
+# This is optimize in time and memory, because you create the
+# things on the fly.
 print('Generator comprehension:')
 a = (x**2 for x in range(10))
 print(a)
