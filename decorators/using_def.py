5a41a7657e41b47f4a3b9b67c59c8098bcaa1813
David Higgins (TUPro) <dave@uiginn.com>
2017-03-29 10:46:49 +0200

    Live coding of simple function-based decorators.

--- decorators/using_def.py
+++ decorators/using_def.py
@@ -1,18 +1,34 @@
+def first_decorator(g):
+    print('this is my first decorator')
+    return g
+
+@first_decorator
+def func_david():
+    print(' inside davids function')
+
+
 def entry_exit(f):
+    # this only gets run once
+    print('once here')
     def new_f():
         print("Entering", f.__name__)
         f()
         print("Exited", f.__name__)
-    # new_f.__name__ = f.__name__
+    # this only gets run once
+    print('running here just once')
+    new_f.__name__ = f.__name__
     return new_f
 
 @entry_exit
 def func1():
-    print("inside func1()")
+    print("    inside func1()")
+
+
+func1()
 
 @entry_exit
 def func2():
-    print("inside func2()")
+    print("     inside func2()")
 
 func1()
 func2()
