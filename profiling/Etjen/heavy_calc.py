04922dfe23c83578755ef07a62e674eec4fba92a
Etjen Ymeraj <etjenymeraj@outlook.com>
2017-04-21 19:00:11 +0200

    Added notes to reflect on the command lines executed

---
+++ profiling/Etjen/heavy_calc.py
@@ -0,0 +1,20 @@
+import cProfile
+
+def f(x):
+    long = range(10**x)
+    y = []
+    for idx in long:
+        y.extend([idx*2])
+        #y = [idx]
+    return y
+
+def g(y):
+    z = []
+    for idx in f(y):
+        z.append(idx/2)
+    return z
+
+pow = 7
+
+a = g(pow)
+print(a)
